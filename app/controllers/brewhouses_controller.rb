113ad33f84e316c896a52a8d8d298142a429eb87
Jack Sanders <Guitarbreakerjs@gmail.com>
2020-10-14 12:06:14 -0600

    users_brewery

--- app/controllers/brewhouses_controller.rb
+++ app/controllers/brewhouses_controller.rb
@@ -1,3 +1,10 @@
 class BrewhousesController < ApplicationController
-    
+    def index 
+        @brewhouses = Brewhouse.all
+        render json: @brewhouses
+    end 
+    def show 
+        @brewhouse = Brewhouse.find(params[:id])
+        render json: @brewhouse 
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+   def create 
+    @user = User.create({
+        name: params[:name], 
+        age: params[:age]
+        })
+    render json: @user
+   end
+   
+   def index
+    @users = Users.all
+    render json: @users
+   end
 end
